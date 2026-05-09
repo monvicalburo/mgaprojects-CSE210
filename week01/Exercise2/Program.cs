bef3d6fdf7fd1bfd235d2241ab8c0db9da186548
monvicalburo <monvicalburo@gmail.com>
2026-05-09 09:32:05 +0800

    changed some code for 2nd core

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -11,27 +11,30 @@ class Program
         if (x >= 90)
         {
             Console.WriteLine("A");
-            Console.WriteLine("Congratulations, you passed the course!");
         }
         else if (x >= 80)
         {
             Console.WriteLine("B");
-            Console.WriteLine("Congratulations, you passed the course!");
         }
         else if (x >= 70)
         {
             Console.WriteLine("C");
-            Console.WriteLine("Congratulations, you passed the course!");
         }
         else if (x >= 60)
         {
             Console.WriteLine("D");
-            Console.WriteLine("You failed the course.");
         }
         else
         {
             Console.WriteLine("F");
-            Console.WriteLine("You failed the course.");
+        }
+        if (x >= 70)
+        {
+            Console.WriteLine("Congratulations, you passed the course!");
+        }
+        else
+        {
+            Console.WriteLine("You failed the course!");
         }
     }
 }
