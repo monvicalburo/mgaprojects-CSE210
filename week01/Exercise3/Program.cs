bc843df58cdc30f6a0ff6509f0a590eefcf81408
monvicalburo <monvicalburo@gmail.com>
2026-05-09 12:20:49 +0800

    completed the 1st core requirement

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -4,6 +4,27 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
+        
+        Console.Write("What is the magic number? ");
+        string magicNumber = Console.ReadLine();
+        int x = int.Parse(magicNumber);
+
+        Console.Write("What is your guess? ");
+        string guess = Console.ReadLine();
+        int y = int.Parse(guess);
+
+        if (y < x)
+        {
+            Console.WriteLine("Higher");
+        }
+        else if (y > x)
+        {
+            Console.WriteLine("Lower");
+        }
+        else
+        {
+            Console.WriteLine("You Guessed it!");
+        }
+
     }
 }
